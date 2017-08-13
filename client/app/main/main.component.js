302123f96d2a7596c853329083161bb3063d8f3b
Nick Ocampo <nick@wehatenick.com>
2017-08-13 14:17:57 -0700

    added highlight block details UI

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -5,7 +5,7 @@ import routing from './main.routes';
 export class MainController {
 
   /*@ngInject*/
-  constructor($timeout) {
+  constructor ($timeout) {
     this.$timeout = $timeout;
   }
 
@@ -31,7 +31,32 @@ export class MainController {
     }
   ];
 
-  $onInit() {
+  itemDetails = {
+    coding: [
+      'ES6',
+      'AngularJS',
+      'React',
+      'HTML/CSS'
+    ],
+    design: [
+      'Sketch',
+      'Flinto',
+      'MarvelApp',
+      'Material Design'
+    ],
+    strategy: [
+      'Usability testing',
+      'Product management',
+      'Behavioral anlytics',
+      'SEO and SEM'
+    ]
+  };
+
+  setDetails (item) {
+    this.details = this.itemDetails[item];
+  }
+
+  $onInit () {
     this.$timeout( () => {
       this.init = true;      
     }, 1500);
